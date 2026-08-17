17c41a4fea4878004c372e550bf09ec634573e3f
Nate Pink <nate.pink@gmail.com>
2026-08-17 20:40:33 +0000

    CVE fix system.text.json 2.0 (#117)
    * CVE System.Text.Json 2.0
    
    * Add default shell to workflows
    
    * Update Xml Docs Comments
    
    * Update to system.text.json 9.x
    
    * AutomaticVersionRange="false"
    
    * dotnet 8
    
    * 8.0.5
    
    ---------
    
    Co-authored-by: Nick Gallegos <nick.gallegos@particular.net>

--- src/TotallyLegit/Calculator.cs
+++ src/TotallyLegit/Calculator.cs
@@ -1,24 +1,46 @@
 ﻿namespace TotallyLegit;
 
+/// <summary>
+/// Calculator
+/// </summary>
 public class Calculator
 {
-    private int _result;
+    int _result;
 
+    /// <summary>
+    /// GetTotal
+    /// </summary>
+    /// <returns></returns>
     public int GetTotal()
     {
         return _result;
     }
 
+    /// <summary>
+    /// Add
+    /// </summary>
+    /// <param name="nextNumber"></param>
+    /// <returns></returns>
     public int Add(int nextNumber)
     {
         return _result += nextNumber;
     }
 
+    /// <summary>
+    /// Subtract
+    /// </summary>
+    /// <param name="nextNumber"></param>
+    /// <returns></returns>
     public int Subtract(int nextNumber)
     {
         return _result -= nextNumber;
     }
 
+    /// <summary>
+    /// Multiply
+    /// </summary>
+    /// <param name="nextNumber"></param>
+    /// <returns></returns>
     public int Multiply(int nextNumber)
     {
         return _result *= nextNumber;
