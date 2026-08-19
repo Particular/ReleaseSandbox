2bb7eb8eb724b6357a6ac59868b74b9df1cf56bb
Nick Gallegos <nick.gallegos@particular.net>
2026-08-19 16:40:51 -0600

    Add project that will output an executable

--- src/TotallyLegit/Calculator.cs
+++ src/TotallyLegit/Calculator.cs
@@ -11,38 +11,26 @@ public class Calculator
     /// Gets the current running total.
     /// </summary>
     /// <returns>The current running total.</returns>
-    public int GetTotal()
-    {
-        return _result;
-    }
+    public int GetTotal() => _result;
 
     /// <summary>
     /// Adds a number to the running total.
     /// </summary>
     /// <param name="nextNumber">The number to add.</param>
     /// <returns>The updated running total.</returns>
-    public int Add(int nextNumber)
-    {
-        return _result += nextNumber;
-    }
+    public int Add(int nextNumber) => _result += nextNumber;
 
     /// <summary>
     /// Subtracts a number from the running total.
     /// </summary>
     /// <param name="nextNumber">The number to subtract.</param>
     /// <returns>The updated running total.</returns>
-    public int Subtract(int nextNumber)
-    {
-        return _result -= nextNumber;
-    }
+    public int Subtract(int nextNumber) => _result -= nextNumber;
 
     /// <summary>
     /// Multiplies the running total by a number.
     /// </summary>
     /// <param name="nextNumber">The number to multiply by.</param>
     /// <returns>The updated running total.</returns>
-    public int Multiply(int nextNumber)
-    {
-        return _result *= nextNumber;
-    }
+    public int Multiply(int nextNumber) => _result *= nextNumber;
 }
